a30eff554ee7757a1b79d828d463ebdd72a4749d
miacruz21 <118496929+miacruz21@users.noreply.github.com>
2024-12-02 13:53:20 +0800

    fix

--- models/main_db.js
+++ models/main_db.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const masterDb = new Sequelize('steamgameswarehouse', 'user', 'password', {
+const masterDb = new Sequelize('dim_gameinfo', 'user', 'password', {
     host: 'ccscloud.dlsu.edu.ph',
     port: 21922,
     dialect: 'mysql'
 });
 
-const nodeOneDb = new Sequelize('steamgameswarehouse', 'user', 'password', {
+const nodeOneDb = new Sequelize('dim_gameinfo', 'user', 'password', {
     host: 'ccscloud.dlsu.edu.ph',
     port: 21932,
     dialect: 'mysql'
 });
 
-const nodeTwoDb = new Sequelize('steamgameswarehouse', 'user', 'password', {
+const nodeTwoDb = new Sequelize('dim_gameinfo', 'user', 'password', {
     host: 'ccscloud.dlsu.edu.ph',
     port: 21942,
     dialect: 'mysql'
